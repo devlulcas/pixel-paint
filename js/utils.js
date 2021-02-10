a918e4d9af6cedfb529c0a364c88f76020cab320
lulcas <lucasalvesrego.contato@gmail.com>
2021-02-10 17:28:21 -0300

    did a little refactoring in the createSquares function and added a function that generates the grid template collumns and rows

--- js/utils.js
+++ js/utils.js
@@ -8,15 +8,20 @@ const getCanvasSize = (inputField, normalSize = 32) => {
 
 const createSquares = (canvasSize = 32) => {
     let squares = ``;
-    for (let lines = 0; lines < canvasSize; lines++){
-        for (let columns = 0; columns < canvasSize; columns++){
-            squares += `<div class="default clean"></div>`;
-        }
-        squares+=`<br>`;
+    for (let squaresCreated = 0; squaresCreated < (canvasSize * canvasSize); squaresCreated++){
+        squares += `<div class="default clean"></div>`;
     }
     return squares;
 }
 
 const createCanvas = (container, squares) => {
     container.innerHTML = squares;
+}
+
+const createGrid = (canvasSize = 32, gridSize = '10px') => {
+    let grid = ``;
+    for (let axis = 0; axis < canvasSize; axis++){
+        grid += ` ${gridSize}`
+    }
+    return grid;
 }
