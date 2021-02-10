1c92ab343cf75eb9894abfb8c79d145fd7b632f5
lulcas <lucasalvesrego.contato@gmail.com>
2021-02-10 17:35:56 -0300

    changed the arrow function to named functions

--- js/utils.js
+++ js/utils.js
@@ -1,27 +1,27 @@
 export {getCanvasSize, createSquares, createCanvas, createGrid};
 
-const getCanvasSize = (inputField, normalSize = 32) => {
+function getCanvasSize(inputField, normalSize = 32) {
     const defaultSize = 32;
-    let canvasSize = inputField.value;   
-    return (canvasSize > 0 ) ? canvasSize : (normalSize > 0) ? normalSize : defaultSize;
+    let canvasSize = inputField.value;
+    return (canvasSize > 0) ? canvasSize : (normalSize > 0) ? normalSize : defaultSize;
 }
 
-const createSquares = (canvasSize = 32) => {
+function createSquares(canvasSize = 32) {
     let squares = ``;
-    for (let squaresCreated = 0; squaresCreated < (canvasSize * canvasSize); squaresCreated++){
+    for (let squaresCreated = 0; squaresCreated < (canvasSize * canvasSize); squaresCreated++) {
         squares += `<div class="default clean"></div>`;
     }
     return squares;
 }
 
-const createCanvas = (container, squares) => {
+function createCanvas(container, squares) {
     container.innerHTML = squares;
 }
 
-const createGrid = (canvasSize = 32, gridSize = '10px') => {
+function createGrid(canvasSize = 32, gridSize = '10px') {
     let grid = ``;
-    for (let axis = 0; axis < canvasSize; axis++){
-        grid += ` ${gridSize}`
+    for (let axis = 0; axis < canvasSize; axis++) {
+        grid += ` ${gridSize}`;
     }
     return grid;
 }
