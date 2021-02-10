7e0b6b11d39188a6801c04016e130d23a11b6897
lulcas <lucasalvesrego.contato@gmail.com>
2021-02-09 23:44:00 -0300

    added the functionality of creating the squares in the container div

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
-import {getCanvasSize} from './utils';
-
+import {getCanvasSize, createSquares, createCanvas} from './utils.js';
 const sizeInputField = document.getElementById('input-canvas-size');
 const btnChangeSize = document.getElementById('btn-canvas-size');
+const container = document.getElementById('container');
+
+btnChangeSize.onclick = () => {
+    const canvasSize = getCanvasSize(sizeInputField, 32);
+    const squares = createSquares(canvasSize);
+    console.log(squares)
+    createCanvas(container, squares);
+}
