75be03960227f5e333e0a0d88cd910a199a3057c
lulcas <lucasalvesrego.contato@gmail.com>
2021-02-09 22:38:11 -0300

    added the createSquares function and the createCanvas function

--- js/utils.js
+++ js/utils.js
@@ -1,7 +1,21 @@
-export {getCanvasSize};
+export {getCanvasSize, createSquares, createCanvas};
 
 const getCanvasSize = (inputField, normalSize = 32) => {
     const defaultSize = 32;
     let canvasSize = inputField.value;   
     return (canvasSize > 0 ) ? canvasSize : (normalSize > 0) ? normalSize : defaultSize;
+}
+
+const createSquares = (canvasSize = 32) => {
+    const squares = ``;
+    for (let i = 0; i <= canvasSize; i++){
+        squares += `
+                <div class="default clean"></div>
+        `;
+    }
+    return squares;
+}
+
+const createCanvas = (container, squares) => {
+    container.innerHTML = squares;
 }
