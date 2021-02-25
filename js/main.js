32744e6888e3c78c0246499340d9236148459699
lulcas <lucasalvesrego.contato@gmail.com>
2021-02-25 11:47:46 -0300

    formated everything with prettier

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
-import {getCanvasSize, createSquares, createCanvas, createGrid, applyGridStyle} from './utils.js';
-const sizeInputField = document.getElementById('input-canvas-size');
-const btnChangeSize = document.getElementById('btn-canvas-size');
-const container = document.getElementById('container');
+import {
+  getCanvasSize,
+  createSquares,
+  createCanvas,
+  createGrid,
+  applyGridStyle,
+} from "./utils.js";
+const sizeInputField = document.getElementById("input-canvas-size");
+const btnChangeSize = document.getElementById("btn-canvas-size");
+const container = document.getElementById("container");
 
 let actualCanvasSize = 0;
 btnChangeSize.onclick = () => {
-    const canvasSize = getCanvasSize(sizeInputField, 8, 32, 64);
-    if (canvasSize != actualCanvasSize){
-        actualCanvasSize = canvasSize;
-        const squares = createSquares(canvasSize);
-        const gridTemplate = createGrid(canvasSize, '1em');
-        createCanvas(container, squares);
-        applyGridStyle(gridTemplate, container);
-    }
-}
+  const canvasSize = getCanvasSize(sizeInputField, 8, 32, 64);
+  if (canvasSize != actualCanvasSize) {
+    actualCanvasSize = canvasSize;
+    const squares = createSquares(canvasSize);
+    const gridTemplate = createGrid(canvasSize, "1em");
+    createCanvas(container, squares);
+    applyGridStyle(gridTemplate, container);
+  }
+};
--- js/utils.js
+++ js/utils.js
@@ -1,33 +1,51 @@
-export {getCanvasSize, createSquares, createCanvas, createGrid, applyGridStyle};
+export {
+  getCanvasSize,
+  createSquares,
+  createCanvas,
+  createGrid,
+  applyGridStyle,
+};
 
-function getCanvasSize(inputField, minSize = 8, normalSize = 32, maxSize = 128) {
-    const defaultSize = 32;
-    let canvasSize = inputField.value;
-    return (canvasSize >= minSize && canvasSize <= maxSize) ? canvasSize 
-    : (normalSize >= minSize && normalSize <= maxSize) ? normalSize : defaultSize;
+function getCanvasSize(
+  inputFieldId,
+  minSize = 8,
+  normalSize = 32,
+  maxSize = 128
+) {
+  const defaultSize = 32;
+  let canvasSize = inputFieldId.value;
+  return canvasSize >= minSize && canvasSize <= maxSize
+    ? canvasSize
+    : normalSize >= minSize && normalSize <= maxSize
+    ? normalSize
+    : defaultSize;
 }
 
 function createSquares(canvasSize = 32) {
-    let squares = ``;
-    for (let squaresCreated = 0; squaresCreated < (canvasSize * canvasSize); squaresCreated++) {
-        squares += `<div class="default clean"></div>`;
-    }
-    return squares;
+  let squares = ``;
+  for (
+    let squaresCreated = 0;
+    squaresCreated < canvasSize * canvasSize;
+    squaresCreated++
+  ) {
+    squares += `<div class="default clean"></div>`;
+  }
+  return squares;
 }
 
 function createCanvas(container, squares) {
-    container.innerHTML = squares;
+  container.innerHTML = squares;
 }
 
-function createGrid(canvasSize = 32, gridSize = '10px') {
-    let grid = ``;
-    for (let axis = 0; axis < canvasSize; axis++) {
-        grid += ` ${gridSize}`;
-    }
-    return grid;
+function createGrid(canvasSize = 32, gridSize = "10px") {
+  let grid = ``;
+  for (let axis = 0; axis < canvasSize; axis++) {
+    grid += ` ${gridSize}`;
+  }
+  return grid;
 }
 
-function applyGridStyle(gridTemplate = 'auto', container) {
-    container.style.gridTemplateColumns = gridTemplate;
-    container.style.gridTemplateRows = gridTemplate;
+function applyGridStyle(gridTemplate = "auto", container) {
+  container.style.gridTemplateColumns = gridTemplate;
+  container.style.gridTemplateRows = gridTemplate;
 }
